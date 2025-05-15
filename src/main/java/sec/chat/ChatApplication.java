8130ba0ab6a101cbeb8272faf563ee5dd3361c1e
gleb_fert <liant379@gmail.com>
2025-05-16 01:16:24 +0300

    init

---
+++ src/main/java/sec/chat/ChatApplication.java
@@ -0,0 +1,12 @@
+package sec.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class ChatApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(ChatApplication.class, args);
+    }
+}
